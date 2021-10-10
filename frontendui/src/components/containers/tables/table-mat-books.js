0254b3007eb0aa2a524f024bba55256a16fe8115
JayaniHerath98 <jayaniherath98@gmail.com>
2021-10-11 00:47:00 +0530

    ui delete update add ch3

--- frontendui/src/components/containers/tables/table-mat-books.js
+++ frontendui/src/components/containers/tables/table-mat-books.js
@@ -64,7 +64,7 @@ export default function BooksTable() {
     await books_service.updateBook(id, body);
   }
 
-  const deleteOrganization = async (id) => {
+  const deleteBook = async (id) => {
     await books_service.deleteBook(id);
   }
 
